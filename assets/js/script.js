ec9674fe211f2d5c9c954ce620f08ae948f881bf
André Rodrigues <rodrigues.domingos996@gmail.com>
2023-04-05 20:37:23 -0300

    criando animação de digitação

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/* =================  animação da digitação ================= */
+
+
+
+var escrita = new Typed(".principal-info__digitacao",{
+    strings:["","Web Designer", "Web Developer", "Full-stack"],
+    typeSpedd:100,
+    BackSpeed:60,
+    loop:true
+})
