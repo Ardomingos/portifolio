2141c69b4e9c3b0ddfcdfdb05878dc4e7caef9e1
André Rodrigues <rodrigues.domingos996@gmail.com>
2023-04-05 21:53:48 -0300

    criando animações e fazendo a parte de js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,41 @@ var escrita = new Typed(".principal-info__digitacao",{
     BackSpeed:60,
     loop:true
 })
+
+
+
+
+
+/* =================  Barra lateral ================= */
+
+const nav = document.querySelector(".menu-lateral"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".secao"),
+    totalSection = allSection.length;
+    for(let i =0; i<totalNavList; i++)
+    {
+       
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function(){
+            for(let j=0; j<totalNavList; j++){
+                if(navList[j].querySelector("a").classList.contains("active")){
+                    console.log(navList[i.querySelector("a")])
+                }
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active");
+            showSection(this);
+        })
+    }
+
+    function showSection(element){
+
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove("active");
+        }
+
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active")
+        
+    }
